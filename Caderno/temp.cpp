dde1d5db070b566f630d265fd502866d1c96ff1b
raphaelrbr <36173192+raphaelrbr@users.noreply.github.com>
2019-05-04 13:04:59 -0300

    Add files via upload

---
+++ Caderno/temp.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define FOR(i,n) 	for(int i=0;i<n;i++)
+#define FORR(i,n)	for(int i=1;i<=n;i++)
+
+#define MEMO(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define ALL(x)	x.begin(),x.end()
+
+#define endl "\n"
+#define olar cout << "olar" << "\n";
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
+
+#define minpq priority_queue<pii, vector<pii>, greater<pii> >  
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9+7;
+
+int main(){
+
+	fastio;
+
+}
